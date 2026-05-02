021ddf7ec179a114006e7842aec54dad6797a9b6
Kate <katesty1998@gmail.com>
2026-05-02 14:49:29 +0300

    updated vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import injectHTML from "vite-plugin-html-inject";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/best-shop-edu/",
+  base: "/",
   root: "src",
   publicDir: resolve(__dirname, "public"),
   build: {
